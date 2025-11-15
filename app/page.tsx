3707e506b11a2a572c3cd17a8b8f5b8a2912d3f4
destruktor1993 <kavcicborna93@gmail.com>
2025-11-15 14:43:04 +0100

    Explore Events button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Page = () => {
     return (
         <div>
             <section>
                 <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't miss</h1>
                 <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
+                <ExploreBtn/>
             </section>
         </div>
     );
