20a983ebdbd612ff2cef3817b96ff38e66a3288d
destruktor1993 <kavcicborna93@gmail.com>
2025-11-15 14:45:41 +0100

    Place holder for events

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ const Page = () => {
                 <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't miss</h1>
                 <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
                 <ExploreBtn/>
+
+                <div className="events">
+                    <h3>Featured Events</h3>
+
+                    <ul className="events">
+                        {[1, 2, 3, 4, 5].map(event => <li key={event}>Event {event}</li>)}
+                    </ul>
+                </div>
             </section>
         </div>
     );
