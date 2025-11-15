016bb732ed6e4369705c327ff6ee6f9e169a360b
destruktor1993 <kavcicborna93@gmail.com>
2025-11-15 13:24:16 +0100

    Initial Styling, intro page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-
 const Page = () => {
     return (
         <div>
-            Welcome to NextJS 16
+            <section>
+                <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't miss</h1>
+                <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
+            </section>
         </div>
     );
 };
