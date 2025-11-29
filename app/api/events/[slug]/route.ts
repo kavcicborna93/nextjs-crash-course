33cc7669e14ede2bc56945aeb9174c0a7c2b42c4
destruktor1993 <kavcicborna93@gmail.com>
2025-11-29 12:10:11 +0100

    Implementing GET method based on slug identifier

---
+++ app/api/events/[slug]/route.ts
@@ -0,0 +1,30 @@
+import {NextRequest, NextResponse} from "next/server";
+import connectDB from "@/lib/mongodb";
+import Event from "../../../../database/event.model";
+
+type RouteParams = {
+    params: Promise<{slug:string}>;
+}
+export async function GET(req: NextRequest, {params}: RouteParams) {
+    try {
+        await connectDB();
+        const slug = await {params};
+        // return sorted events, newer first //
+        const event = await Event.findOne(slug).lean();
+
+        if (!event) {
+            return NextResponse.json({error: "Event not found"}, {status:400});
+        }
+
+        return NextResponse.json({message: 'Event fetched successfully', event}, {status: 200});
+    } catch (e) {
+        if (e instanceof Error) {
+            if (e.message.includes('MONGODB_URI')) {
+                return NextResponse.json({error:"Database configuration error"});
+            }
+            return NextResponse.json({message: 'Event fetching failed', error: e}, {status: 500});
+        }
+    }
+
+    return NextResponse.json({message: 'An unexpected error occured'}, {status:500});
+}
