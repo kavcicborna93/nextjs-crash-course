fb308eafe619230c34561df24694d130d471e589
destruktor1993 <kavcicborna93@gmail.com>
2025-11-30 18:51:41 +0100

    JSON parsing fix

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -11,11 +11,15 @@ export async function POST(req: NextRequest) {
         const formData = await req.formData();
 
         let event;
+        let tags;
+        let agenda;
 
         try {
             event = Object.fromEntries(formData.entries());
+            tags = JSON.parse(formData.get('tags') as string);
+            agenda = JSON.parse(formData.get('agenda') as string);
         } catch (e) {
-            return NextResponse.json({message: 'Invalid JSON format'}, {status: 400})
+            return NextResponse.json({message: 'Invalid JSON format in request data'}, {status: 400});
         }
 
         const file = formData.get('image') as File;
@@ -23,9 +27,6 @@ export async function POST(req: NextRequest) {
         if (!file) {
             return NextResponse.json({message: 'Image file is required'}, {status: 400});
         }
-
-        let tags = JSON.parse(formData.get('tags') as string);
-        let agenda = JSON.parse(formData.get('agenda') as string);
 
         const arrayBuffer = await file.arrayBuffer();
         const buffer = Buffer.from(arrayBuffer);
